46033126127cdb645d035bf9a835c4722c1ff04c
fvonoronha <fabio.vitor.noronha@gmail.com>
2026-03-20 16:20:07 -0400

    fix: tive que colocar o endereço da API hardcoded. no futuro eu resolvo essa questão do Next.js

--- src/utils/api/call/index.tsx
+++ src/utils/api/call/index.tsx
@@ -2,15 +2,28 @@ import axios, { GenericAbortSignal } from "axios";
 import { APIResponseType } from "types";
 import { getStorage } from "utils";
 
+/// O Next.js nao expõe o .env para a parte que roda no cliente.
+// ToDo: tratar isso pois devem haver maneiras de lidar com isso para que funcione
+// sem precisar passa nada hardcoded
+// const api = axios.create({
+//     baseURL: process.env.NEXT_PUBLIC_API_HOST,
+//     timeout: parseInt(process.env.NEXT_PUBLIC_API_CALL_TIMEOUT || "60000")
+// });
+
+// TODO: ESSAS CONSTANTES PODEM IR PARA ALGUM UTIL
+const API_CALL_TIMEOUT_2_MIN_IN_MS = 120000;
+const AUTH_USER_TOKEN_NAME = "usrtkn";
+
+/// Gambiarra: Isso aqui dói, mas por hora vai ter que ficar assim. Nao posos atrasar ainda mais o tempo de desenvolvimento por isso
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_HOST,
-    timeout: parseInt(process.env.NEXT_PUBLIC_API_CALL_TIMEOUT || "60000")
+    baseURL: "https://prd.api.biblioteca.feal.espirita.casa", // Ai ai ai
+    timeout: API_CALL_TIMEOUT_2_MIN_IN_MS
 });
 
 api.interceptors.request.use(
     (config) => {
         try {
-            const token = getStorage("usrtkn");
+            const token = getStorage(AUTH_USER_TOKEN_NAME);
             if (token && !config.headers["Authorization"]) {
                 config.headers["Authorization"] = `${token}`;
             }
